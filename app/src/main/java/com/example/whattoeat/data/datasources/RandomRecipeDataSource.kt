5a64c662f8b4cf8c18470b1c0ce23a938c76e131
Alexlegrand1997 <alex_legrand1997@hotmail.fr>
2024-07-16 15:04:34 -0400

    DataSource and Repository created for RandomRecipe

---
+++ app/src/main/java/com/example/whattoeat/data/datasources/RandomRecipeDataSource.kt
@@ -0,0 +1,29 @@
+package com.example.whattoeat.data.datasources
+
+
+import com.example.whattoeat.BuildConfig.SPOONACULAR_API_KEY
+import com.example.whattoeat.core.Constants
+import com.example.whattoeat.models.Recipe
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.json.responseJson
+import com.github.kittinunf.result.Result
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+
+class RandomRecipeDataSource {
+
+    private val json = Json { ignoreUnknownKeys = true }
+
+    fun getOne(): Recipe{
+        val(_,_,result) = Constants.RANDOM_RECIPE.httpGet().appendHeader("x-api-key",
+            SPOONACULAR_API_KEY
+        ).responseJson()
+
+        when (result){
+            is Result.Success -> return json.decodeFromString(result.value.content)
+            is Result.Failure -> throw result.error.exception
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/whattoeat/data/repositories/RandomRecipeRepository.kt
@@ -0,0 +1,27 @@
+package com.example.whattoeat.data.repositories
+
+import com.example.whattoeat.core.ApiResult
+import com.example.whattoeat.data.datasources.RandomRecipeDataSource
+import com.example.whattoeat.models.Recipe
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class RandomRecipeRepository {
+
+    private val _randomRecipeDataSource =RandomRecipeDataSource()
+
+    fun retrieveOne() : Flow<ApiResult<Recipe>>{
+        return flow{
+            emit((ApiResult.Loading))
+            try{
+                emit(ApiResult.Success(_randomRecipeDataSource.getOne()))
+            }
+            catch (ex:Exception){
+                emit(ApiResult.Error(ex))
+            }
+        }.flowOn(Dispatchers.IO)
+    }
+
+}
