309fbc6e3086123072e237b1a7ab3f679fed0010
Alexlegrand1997 <alex_legrand1997@hotmail.fr>
2024-07-18 13:48:04 -0400

    Made some upgrade to the random recipe tab. Srollable ingredient list, added a button to swap between Metric and US value

--- app/src/main/java/com/example/whattoeat/ui/theme/screens/randomRecipe/RandomRecipeScreen.kt
+++ app/src/main/java/com/example/whattoeat/ui/theme/screens/randomRecipe/RandomRecipeScreen.kt
@@ -1,9 +1,16 @@
 package com.example.whattoeat.ui.theme.screens.randomRecipe
 
 import android.widget.Toast
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
@@ -12,27 +19,49 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.FilterQuality
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.ParentDataModifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -41,9 +70,13 @@ import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
 import coil.size.Size
 import com.example.whattoeat.R
+import com.example.whattoeat.core.Constants
+import com.example.whattoeat.models.ExtendedIngredient
 import com.example.whattoeat.models.Recipe
 import com.example.whattoeat.models.Recipes
 import com.example.whattoeat.ui.theme.composables.LoadingSpinner
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+import kotlin.contracts.Returns
 
 @Composable
 fun RandomRecipeScreen(
@@ -70,44 +103,263 @@ fun RandomRecipeScreen(
 @Composable
 fun RandomRecipeScreenCard(recipes: Recipes, randomRecipeViewModel: RandomRecipeViewModel) {
     Surface(modifier = Modifier.fillMaxSize()) {
-
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(top = 20.dp)
-        ) {
-            SubcomposeAsyncImage(
-                model =recipes.recipes[0].image,
-                contentScale = ContentScale.FillWidth,
-                modifier = Modifier.fillMaxWidth(0.9f).
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(top = 20.dp)
+            ) {
+                SubcomposeAsyncImage(
+                    model = recipes.recipes[0].image,
+                    contentScale = ContentScale.FillWidth,
+                    modifier = Modifier
+                        .fillMaxWidth(0.9f)
+                        .
 //                aspectRatio(16/9f).
-                    clip(RoundedCornerShape(20.dp)),
-                contentDescription = recipes.recipes[0].title,
-                loading = {
-                    Box(contentAlignment = Alignment.Center, modifier = Modifier.height(225.dp)){
-                        CircularProgressIndicator(modifier = Modifier.size(60.dp))
+                        clip(RoundedCornerShape(20.dp)),
+                    contentDescription = recipes.recipes[0].title,
+                    loading = {
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier.height(225.dp)
+                        ) {
+                            CircularProgressIndicator(modifier = Modifier.size(60.dp))
+                        }
+                    }, error = {
+                        Image(
+                            painter = painterResource(id = R.drawable.placeholder),
+                            contentDescription = stringResource(id = R.string.picture_unavailable)
+                        )
                     }
-                }, error = {
-                    Image(
-                        painter = painterResource(id = R.drawable.placeholder),
-                        contentDescription = stringResource(id = R.string.picture_unavailable)
-                    )
+                )
+                Text(text = recipes.recipes[0].title)
+            }
+
+            val items = remember {
+                listOf("Metric", "US")
+            }
+            var selectedIndex by remember {
+                mutableStateOf(0)
+            }
+
+            TextSwitch(selectedIndex = selectedIndex, items = items, onSelectionChange = {
+                selectedIndex = it
+            })
+
+
+//            var checked by remember { mutableStateOf(true) }
+//
+//            Switch(
+//                checked = checked,
+//                onCheckedChange = {
+//                    checked = it
+//
+//                }
+//            )
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.75f),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                items(recipes.recipes[0].extendedIngredients) { ingredient ->
+                    ingredientCard(ingredient, selectedIndex)
                 }
-            )
-            Text(text = recipes.recipes[0].title)
-        }
+            }
 
 
-
-        Row(verticalAlignment = Alignment.Bottom, horizontalArrangement = Arrangement.Center) {
-            Button(onClick = { refreshRecipe(randomRecipeViewModel) }) {
-                Text(text = "New Recipe")
+            Row(verticalAlignment = Alignment.Bottom, horizontalArrangement = Arrangement.Center) {
+                Button(onClick = { refreshRecipe(randomRecipeViewModel) }) {
+                    Text(text = "New Recipe")
+                }
             }
         }
-
     }
 }
 
-fun refreshRecipe(randomRecipeViewModel: RandomRecipeViewModel) {
+private fun refreshRecipe(randomRecipeViewModel: RandomRecipeViewModel) {
     randomRecipeViewModel.getRandomRecipe()
 }
 
+
+@Composable
+fun ingredientCard(ingredient: ExtendedIngredient, typeMeasure: Int) {
+    Card(
+        Modifier
+            .fillMaxWidth(0.95f)
+            .padding(top = 4.dp)
+    ) {
+        Row(modifier = Modifier.height(75.dp), verticalAlignment = Alignment.CenterVertically) {
+            Column(
+                Modifier.fillMaxWidth(0.3f),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                loadImageIngredient(ingredient)
+
+            }
+
+            Column(Modifier.padding(start = 8.dp)) {
+                Text(ingredient.name)
+                // True is US
+                if (typeMeasure==1) {
+                    Text("${ingredient.measures.us.amount} ${ingredient.measures.us.unitLong}")
+                }
+                // False is Metric
+                else {
+                    Text("${ingredient.measures.metric.amount} ${ingredient.measures.metric.unitLong}")
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun loadImageIngredient(ingredient: ExtendedIngredient) {
+    SubcomposeAsyncImage(
+        model = Constants.URL_CDN_INGREDIENT + ingredient.image,
+        contentScale = ContentScale.FillHeight,
+        modifier = Modifier
+            .clip(RoundedCornerShape(8.dp))
+            .fillMaxHeight(0.8f),
+        contentDescription = ingredient.name,
+        filterQuality = FilterQuality.High,
+        loading = {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.height(225.dp)
+            ) {
+                CircularProgressIndicator(modifier = Modifier.size(60.dp))
+            }
+        }, error = {
+            Image(
+                painter = painterResource(id = R.drawable.placeholder),
+                contentDescription = stringResource(id = R.string.picture_unavailable)
+            )
+        }
+    )
+}
+
+
+@Composable
+fun SwitchUSOrMetric() {
+    var checked by remember { mutableStateOf(true) }
+
+    Switch(
+        checked = checked,
+        onCheckedChange = {
+            checked = it
+
+        }
+    )
+}
+
+
+// Try to make a nice switch
+// Reference : https://stackoverflow.com/questions/77330319/how-to-create-switcher-with-two-string-states
+fun ContentDrawScope.drawWithLayer(block: ContentDrawScope.() -> Unit) {
+    with(drawContext.canvas.nativeCanvas) {
+        val checkPoint = saveLayer(null, null)
+        block()
+        restoreToCount(checkPoint)
+    }
+}
+
+@Composable
+private fun TextSwitch(
+    modifier: Modifier = Modifier,
+    selectedIndex: Int,
+    items: List<String>,
+    onSelectionChange: (Int) -> Unit
+) {
+
+    BoxWithConstraints(
+        modifier
+            .padding(8.dp)
+            .height(56.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .background(Color(0xfff3f3f2))
+            .padding(8.dp)
+    ) {
+        if (items.isNotEmpty()) {
+
+            val maxWidth = this.maxWidth
+            val tabWidth = maxWidth / items.size
+
+            val indicatorOffset by animateDpAsState(
+                targetValue = tabWidth * selectedIndex,
+                animationSpec = tween(durationMillis = 250, easing = FastOutSlowInEasing),
+                label = "indicator offset"
+            )
+
+            // This is for shadow layer matching white background
+            Box(
+                modifier = Modifier
+                    .offset(x = indicatorOffset)
+                    .shadow(4.dp, RoundedCornerShape(8.dp))
+                    .width(tabWidth)
+                    .fillMaxHeight()
+            )
+
+            Row(modifier = Modifier
+                .fillMaxWidth()
+
+                .drawWithContent {
+
+                    // This is for setting black tex while drawing on white background
+                    val padding = 8.dp.toPx()
+                    drawRoundRect(
+                        topLeft = Offset(x = indicatorOffset.toPx() + padding, padding),
+                        size = androidx.compose.ui.geometry.Size(
+                            size.width / 2 - padding * 2,
+                            size.height - padding * 2
+                        ),
+                        color = Color.Black,
+                        cornerRadius = CornerRadius(x = 8.dp.toPx(), y = 8.dp.toPx()),
+                    )
+
+                    drawWithLayer {
+                        drawContent()
+
+                        // This is white top rounded rectangle
+                        drawRoundRect(
+                            topLeft = Offset(x = indicatorOffset.toPx(), 0f),
+                            size = androidx.compose.ui.geometry.Size(
+                                size.width / 2,
+                                size.height
+                            ),
+                            color = Color.White,
+                            cornerRadius = CornerRadius(x = 8.dp.toPx(), y = 8.dp.toPx()),
+                            blendMode = BlendMode.SrcOut
+                        )
+                    }
+
+                }
+            ) {
+                items.forEachIndexed { index, text ->
+                    Box(
+                        modifier = Modifier
+                            .width(tabWidth)
+                            .fillMaxHeight()
+                            .clickable(
+                                interactionSource = remember {
+                                    MutableInteractionSource()
+                                },
+                                indication = null,
+                                onClick = {
+                                    onSelectionChange(index)
+                                }
+                            ),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = text,
+                            fontSize = 20.sp,
+                            color = Color.Gray
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
