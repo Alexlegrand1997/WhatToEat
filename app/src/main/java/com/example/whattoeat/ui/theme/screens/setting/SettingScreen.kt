d4e6c6308de14863ace76aeaec079b9613fad0dd
Alexlegrand1997 <alex_legrand1997@hotmail.fr>
2024-08-09 16:00:58 -0400

    - UI Button for theme changing in setting working for phone in portrait and in landscape

--- app/src/main/java/com/example/whattoeat/ui/theme/screens/setting/SettingScreen.kt
+++ app/src/main/java/com/example/whattoeat/ui/theme/screens/setting/SettingScreen.kt
@@ -1,17 +1,12 @@
 package com.example.whattoeat.ui.theme.screens.setting
 
 import android.widget.Toast
-import androidx.compose.foundation.layout.Row
+
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.selection.selectable
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -20,10 +15,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.example.whattoeat.ui.theme.composables.LoadingSpinner
+import com.example.whattoeat.ui.theme.screens.setting.components.themeChoice
 
 @Composable
 fun SettingScreen(settingViewModel: SettingViewModel) {
@@ -45,82 +40,30 @@ fun SettingScreen(settingViewModel: SettingViewModel) {
         }
 
         is SettingUIState.Success -> {
-
             themeLocation = if (state.themeValue.isNullOrEmpty()) {
                 2
             } else {
                 themes.indexOf(state.themeValue)
             }
-            Surface(modifier = Modifier.fillMaxSize()) {
-                themeChoice(
-                    modifier = Modifier,
-                    optionsList = themes,
-                    defaultSelected = themeLocation,
-                    settingViewModel
-                )
-
-            }
-        }
-    }
-
-
-}
-
-
-@Composable
-fun themeChoice(
-    modifier: Modifier,
-    optionsList: List<String>,
-    defaultSelected: Int,
-    settingViewModel: SettingViewModel
-) {
-    var selectedOption by remember {
-        mutableIntStateOf(defaultSelected)
-    }
-    LazyRow(
-        modifier
-            .fillMaxWidth()
-            .height(30.dp)
-    ) {
-        items(optionsList) {
-            if (it == optionsList[selectedOption]) {
-//                homeViewModel.handleScreenEvents(SettingsScreenEvent.SetNewTheme(themes[selectedOption]))
-            }
-            ButtonForDialog(
-                modifier = modifier, it, optionsList[selectedOption]
-            ) { selectedValue ->
-                selectedOption = optionsList.indexOf(selectedValue)
-                settingViewModel.handleScreenEvents(SettingsScreenEvent.SetNewTheme(themes[selectedOption]))
-            }
-
+            settings(themeLocation,settingViewModel)
         }
     }
 }
 
 @Composable
-fun ButtonForDialog(
-    modifier: Modifier,
-    text: String,
-    selectedValue: String,
-    onClickListener: (String) -> Unit
-) {
+fun settings(themeLocation:Int,settingViewModel: SettingViewModel) {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(Modifier.fillMaxWidth().padding(start = 0.dp, top = 16.dp, end = 0.dp, bottom = 0.dp),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            themeChoice(
+                modifier = Modifier,
+                optionsList = themes,
+                defaultSelected = themeLocation,
+                settingViewModel
+            )
 
-    Row(
-        modifier
-            .fillMaxWidth()
-            .selectable(selected = (text == selectedValue), onClick = {
-                onClickListener(text)
-            }),
-        verticalAlignment = Alignment.CenterVertically,
+        }
 
-        ) {
-
-        Text(
-            text = text,
-            style = MaterialTheme.typography.titleSmall,
-            modifier = modifier.padding(start = 2.dp),
-            color = if (text == selectedValue) Color.Red else Color.Black
-        )
     }
 }
 
@@ -128,7 +71,7 @@ fun ButtonForDialog(
 enum class ThemeValues(val title: String) {
     LIGHT_MODE("Light Mode"),
     DARK_MODE("Dark Mode"),
-    SYSTEM_DEFAULT("System Default")
+    SYSTEM_DEFAULT("System")
 }
 
 val themes = listOf(
---
+++ app/src/main/java/com/example/whattoeat/ui/theme/screens/setting/components/themeChoice.kt
@@ -0,0 +1,109 @@
+package com.example.whattoeat.ui.theme.screens.setting.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import com.example.whattoeat.ui.theme.screens.setting.SettingViewModel
+import com.example.whattoeat.ui.theme.screens.setting.SettingsScreenEvent
+import com.example.whattoeat.ui.theme.screens.setting.themes
+
+
+@Composable
+fun themeChoice(
+    modifier: Modifier,
+    optionsList: List<String>,
+    defaultSelected: Int,
+    settingViewModel: SettingViewModel
+) {
+    var selectedOption by remember {
+        mutableIntStateOf(defaultSelected)
+    }
+
+    Column(
+        modifier
+            .fillMaxWidth(0.95f)
+            .wrapContentHeight()
+    ) {
+
+        LazyRow(
+            Modifier
+                .border(1.dp,MaterialTheme.colorScheme.background, CircleShape)
+                .clip( shape = CircleShape),
+
+//            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            userScrollEnabled = false
+        )
+        {
+            items(optionsList) {
+                ButtonForDialog(
+                    modifier = Modifier
+                        .height(48.dp)
+                        .fillParentMaxWidth(1 / 3f),
+                    it,
+                    optionsList[selectedOption]
+                ) { selectedValue ->
+                    selectedOption = optionsList.indexOf(selectedValue)
+                    settingViewModel.handleScreenEvents(SettingsScreenEvent.SetNewTheme(themes[selectedOption]))
+                }
+            }
+        }
+
+    }
+}
+
+
+@Composable
+fun ButtonForDialog(
+    modifier: Modifier,
+    text: String,
+    selectedValue: String,
+    onClickListener: (String) -> Unit,
+) {
+    Column(
+        modifier = modifier
+            .selectable(selected = (text == selectedValue), onClick = {
+                onClickListener(text)
+            })
+            .background(
+                if (text == selectedValue) MaterialTheme.colorScheme.primaryContainer else
+                    MaterialTheme.colorScheme.outline
+            ).
+            border(1.dp,MaterialTheme.colorScheme.background),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleSmall,
+        )
+    }
+}
+
+
+
